10ef3724b75c86df3edc689c5f2622b63a0d0bb5
Tzuyi Yang <a0988426059@gmail.com>
2020-04-15 13:56:44 +0800

    Initialize settings

---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fail">
+    <h1>404 Page Not Found</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fail {
+  padding: 100px 0px;
+  margin-bottom: 200px;
+  text-align: center;
+}
+</style>
